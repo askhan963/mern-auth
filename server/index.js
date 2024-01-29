3c89527bf8e3b34f3d9368d10c316872c39e7dc5
Muhammad Awais Khan <95935530+askhan963@users.noreply.github.com>
2024-01-29 16:53:18 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,30 +1,36 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const app = express();
-require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const authRoute = require("./Routes/AuthRoute");
 const { MONGO_URL, PORT } = process.env;
 
+const app = express();
 
-// console.log('MONGO_URL:', process.env.MONGO_URL);
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000"],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
+
+
+// Allow all origins
+app.use(cors());
+
+app.use(cookieParser());
+app.use(express.json());
+app.use("/", authRoute);
+
 mongoose
   .connect(MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("MongoDB is  connected successfully"))
+  .then(() => console.log("MongoDB is connected successfully"))
   .catch((err) => console.error(err));
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
-
-app.use(helmet());
-app.use(cors());
-app.use(cookieParser());
-
-app.use(express.json());
-
-app.use("/", authRoute);
